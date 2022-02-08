dff974dc118dfbfbc875f6e0c932f14fee861996
david95j2 <glory20220j@gmail.com>
2022-02-08 20:40:01 +0900

    init model

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# faster_R_CNN
+# Faster R-CNN Implementation
+
+### Towards Real-Time Object Detection with Region Proposal Networks(https://arxiv.org/pdf/1506.01497.pdf)
+
+---
+---
---
+++ source/main.py
@@ -0,0 +1,23 @@
+# import library
+import numpy
+import torch
+
+# Data Preprocessing
+# data augementation
+# load dataset
+
+# Define Feature extractor
+
+# Generate Anchors Boxes
+
+# Target Anchors
+
+# RPN(Region Proposal Network)
+
+# Proposal layer
+
+# Proposal Target layer
+
+# RoI pooling
+
+# Faster R-CNN
